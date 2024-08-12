0021392c65a22ced63368ebe4313bbce0baa29ac
tejeet <tejeet.magar@nobroker.in>
2024-08-12 18:48:33 +0530

    Added Port

--- config.php
+++ config.php
@@ -1,18 +1,20 @@
 <?php
-define('DB_SERVER','mysql');
-define('DB_USER','root');
-define('DB_PASS' ,'myRoot@554414');
+define('DB_SERVER', '94.136.185.117'); // Change to your MySQL server's hostname or IP address
+define('DB_PORT', '3306');    // Default MySQL port is 3306
+define('DB_USER', 'root');
+define('DB_PASS', 'myRoot@554414');
 define('DB_NAME', 'echo.fleetsapi.com');
 
-define('DIR','https://workdesk.techpulsesolution.com/');
-define('TechPulse','techpulsesolution@gmail.com');
+define('DIR', 'https://workdesk.techpulsesolution.com/');
+define('TechPulse', 'techpulsesolution@gmail.com');
 
-$con = new mysqli(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Create a new mysqli connection
+$con = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Check connection
-if (mysqli_connect_errno()){
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
 }
 
-
+echo "Connected successfully";
 ?>
