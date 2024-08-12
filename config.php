12888d1c9ef5de8bdcaf4d378937327fc793a035
tejeet <tejeet.magar@nobroker.in>
2024-08-12 15:24:42 +0530

    Added DB Files

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'myRot@554414');
+define('DB_NAME', 'echo.fleetsapi.com');
+
+define('DIR','https://workdesk.techpulsesolution.com/');
+define('TechPulse','techpulsesolution@gmail.com');
+
+$con = new mysqli(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+?>
