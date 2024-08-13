7e2e77e3cb4dbbc9f0277833bfe25c5f0f09a4fb
tejeet <tejeet.magar@nobroker.in>
2024-08-13 17:48:55 +0530

    Added Else loop in DB Function

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ $con = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 // Check connection
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
+} else {
+    echo "Connection Failed";
 }
-
-echo "Connected successfully";
 ?>
