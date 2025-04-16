e4683a710ef599df511528d7d77b9136577c2a5a
tejeet <tejeet.magar@nobroker.in>
2025-04-16 16:46:57 +0530

    Added DB Details' '

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+include("config.php");
+
+
+// Generate a random number (can be adjusted to your needs)
+$randomUser = rand(1000, 9999);
+
+// Insert into users table
+$sql = "INSERT INTO users (user, createdon) VALUES ('$randomUser', '$randomUser')";
+
+if ($con->query($sql) === TRUE) {
+    echo "Random user $randomUser added successfully.<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
 
 echo "I am Demo PHP Page";
 
